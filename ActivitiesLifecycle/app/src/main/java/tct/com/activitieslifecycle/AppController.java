436e6f6cde3f67e1e2f0940c5a4d7fcaa7cab302
Thang Truong <tcthang.dsna@gmail.com>
2015-09-17 10:41:07 +0700

    use MyActivityLifecycleCallbacks to manage activities's lifecycle

---
+++ ActivitiesLifecycle/app/src/main/java/tct/com/activitieslifecycle/AppController.java
@@ -0,0 +1,67 @@
+package tct.com.activitieslifecycle;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Copyright © 2015 AsianTech inc.
+ * Created by Thang Truong on 9/17/15.
+ */
+public class AppController extends Application {
+
+    private AppStatus appStatus;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new MyActivityLifecycleCallbacks());
+    }
+
+    public boolean isForeground() {
+        return appStatus != null && appStatus.ordinal() > AppStatus.BACKGROUND.ordinal();
+    }
+
+    /**
+     * This class is used to manage all activities lifecycle in application.
+     */
+    private class MyActivityLifecycleCallbacks implements ActivityLifecycleCallbacks {
+
+        private int numRunningActivity;
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+            if (++numRunningActivity >= 1) {
+                appStatus = AppStatus.FOREGROUND;
+            }
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+            if (--numRunningActivity == 0) {
+                appStatus = AppStatus.BACKGROUND;
+            }
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+        }
+    }
+
+}
